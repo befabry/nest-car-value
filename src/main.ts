f4e8b3ab6ffefe9b6d6703cc027e336bc9a4cd91
b.fabry <fabrybenjamin5@gmail.com>
2021-08-24 16:35:31 +0200

    Auth - signin + signup - part I

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['sadhazjkhda'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/users/auth.service.ts
@@ -0,0 +1,55 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    //See if email is in use
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+    //Hash users password
+    //Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    //Hash the salt and the password
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    //Join the hashed result and the salt together
+    const result = `${salt}.${hash.toString('hex')}`;
+
+    //Create a new user and save it
+    const user = await this.userService.create(email, result);
+
+    //Return the user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.userService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not foud');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password or email');
+    }
+
+    return user;
+  }
+}
