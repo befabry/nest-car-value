cae619d0e45b6799cfe00c2105f55f247c8d3fdc
b.fabry <fabrybenjamin5@gmail.com>
2021-08-31 18:38:05 +0200

    E2E testing

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['sadhazjkhda'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication system (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const requestEmail = 'askdkdjek1@app.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: requestEmail, password: 'jkjkj' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(requestEmail);
+      });
+  });
+});
