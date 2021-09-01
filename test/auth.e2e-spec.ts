bdf88932c012cbbee7e3d7030ed2e80255371cd3
b.fabry <fabrybenjamin5@gmail.com>
2021-09-01 18:19:05 +0200

    Reports bootstraping

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,16 +16,34 @@ describe('Authentication system (e2e)', () => {
   });
 
   it('handles a signup request', () => {
-    const requestEmail = 'askdkdjek1@app.com';
+    const signupEmail = 'asdf@asdf.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email: requestEmail, password: 'jkjkj' })
+      .send({ email: signupEmail, password: 'asdf' })
       .expect(201)
       .then((res) => {
         const { id, email } = res.body;
         expect(id).toBeDefined();
-        expect(email).toEqual(requestEmail);
+        expect(email).toEqual(signupEmail);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
