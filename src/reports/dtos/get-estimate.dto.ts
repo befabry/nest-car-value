f7232e61ba2204e1f7e28bdaf843f84c23e5e4e0
befabry <fabrybenjamin5@gmail.com>
2021-09-09 23:50:57 +0200

    Create a report

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -6,32 +6,32 @@ import {
   IsLongitude,
   IsLatitude,
 } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
 
-export class CreateReportDto {
+export class GetEstimateDto {
   @IsString()
   make: string;
 
   @IsString()
   model: string;
 
+  @Transform(({ value }: TransformFnParams) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1_000_000)
   mileage: number;
 
+  @Transform(({ value }: TransformFnParams) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }: TransformFnParams) => parseFloat(value))
   @IsLatitude()
   lat: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(1_000_000)
-  price: number;
 }
